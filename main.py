b704e01fd667ba59bfb5c3d84baa6d9a8b052c6b
Noah Syrkis <noah@syrkis.com>
2025-07-08 17:42:18 +0200

    include actions in seq output

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 from functools import partial
 from typing import Tuple
 import jax.numpy as jnp
-from einops import repeat
+from einops import repeat, rearrange
 from jax import lax, random, tree, vmap, jit
 from jaxtyping import Array
 from parabellum.env import Env
@@ -16,11 +16,11 @@ import nebellum as nb
 
 
 # %% Constants
-c = 10  # chunks (how many times to reeval plan)
-k = 11  # number of imagined futures
-m = 12  # number of steps into the future to imagine
-n = 13  # total number of steps in a real sim
-s = 14  # number of parallel real sims to run
+c: int = 10  # chunks (how many times to reeval plan)
+k: int = 2  # number of imagined futures
+m: int = 4  # number of steps into the future to imagine
+n: int = 100  # total number of steps in a real sim
+s: int = 2  # number of parallel real sims to run
 env, cfg = Env(), Config(sims=s, steps=n, knn=5)
 
 # %% Config #####################################################
@@ -61,15 +61,16 @@ def step_fn(env: Env, cfg, behavior, carry: Tuple[Obs, State], rng) -> Tuple[Tup
 def chunk_fn(env: Env, cfg: Config, carry: Tuple[Obs, State], rng):
     behavior = nb.act.plan_fn(rng, bts, plan, carry[1])  # perhaps only update plan every m steps
     rngs = random.split(rng, cfg.steps // c)
-    (obs, state), (seq, action) = lax.scan(partial(step_fn, env, cfg, behavior), carry, rngs)
+    (obs, state), seq = lax.scan(partial(step_fn, env, cfg, behavior), carry, rngs)
     aux = lambda x: tree.map(lambda leaf: repeat(leaf, f"... -> {k} ..."), x)  # noqa
     init: Tuple[Obs, State] = aux(obs), aux(encode_fn(cfg, rng, state)[1])
-    (_), (sim_seq, sim_action) = lax.scan(vmap(partial(step_fn, env, cfg, behavior)), init, random.split(rng, (m, k)))
-    return (obs, state), ((seq, action), (sim_seq, sim_action))
+    rngs = random.split(rng, (m, k))
+    sim_seq: Tuple[State, Action] = lax.scan(vmap(partial(step_fn, env, cfg, behavior)), init, rngs)[1]
+    return (obs, state), (seq, sim_seq)
 
 
 def encode_fn(cfg: Config, rng, state: State) -> Tuple[str, State, Array]:
-    mask = random.bernoulli(rng, 0.5, shape=(cfg.length,))
+    mask = random.bernoulli(rng, 0.0, shape=(cfg.length,))
     mean = jnp.where(~mask[:, None], state.pos, 0).sum(0) / (~mask).sum()
     pos = jnp.where(mask[:, None], mean, state.pos)
     hp = jnp.where(mask, jnp.where(~mask, state.hp, 0).sum() / (~mask).sum(), state.hp)
@@ -84,10 +85,26 @@ def traj_fn(env: Env, cfg: Config, obs: Obs, state: State, rng: Array):
     return carry, (seq, sim_seq)  # , action, sim_seq, sim_action)
 
 
+def plot_fn(seq, sim_seq):
+    # c: int = 10  # chunks (how many times to reeval plan)
+    # k: int = 2  # number of imagined futures
+    # m: int = 4  # number of steps into the future to imagine
+    # n: int = 100  # total number of steps in a real sim
+    # s: int = 2  # number of parallel real sims to run
+    print(tree.map(jnp.shape, sim_seq))
+    # seq = tree.map(lambda x: rearrange(x, "s c m k ... -> s (c m k) ...")[:1], seq)
+    sim_seq = tree.map(lambda x: rearrange(x, "s c m k ... -> s (c m k) ...")[:2], sim_seq)
+    # pb.utils.svg_fn(
+    # cfg, seq[0], seq[1], fname="/Users/nobr/desk/s3/nebellum/sims.svg", fps=4, debug=False, targets=points
+    # )
+    pb.utils.svg_fn(
+        cfg, sim_seq[0], sim_seq[1], fname="/Users/nobr/desk/s3/nebellum/sims_sim.svg", fps=4, targets=points
+    )
+
+
 # %%
 key_init, rng_traj = random.split(rng, (2, cfg.sims))
 plan: nb.types.Plan = tree.map(lambda *x: jnp.stack(x), *tuple(map(partial(nb.lxm.str_to_plan, pln_str, cfg), (-1, 1))))  # type: ignore
 obs, state = vmap(partial(env.init, cfg))(key_init)
 (obs, state), (seq, sim_seq) = vmap(partial(traj_fn, env, cfg))(obs, state, rng_traj)
-print(tree.map(jnp.shape, sim_seq[0]))
-# pb.utils.svg_fn(cfg, seq, fname="/Users/nobr/desk/s3/aic2sim/sims.svg", fps=4, debug=False, targets=points)
+plot_fn(seq, sim_seq)
